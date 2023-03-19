550b570bf7b821e45e444c51c2f69b5811c1698a
Ignas Kavaliauskas <ignas.kavaliauskas5@gmail.com>
2023-03-19 22:22:31 +0200

    refactor hooks

--- src/app/pages/home/home-page.tsx
+++ src/app/pages/home/home-page.tsx
@@ -16,66 +16,6 @@ export const useNavigation = (totalPages: number) => {
     const { pageNumber: pageNumberUrlParam } = useParams();
     const [activePageNumber, setActivePageNumber] = useState<number>(1);
 
-    useEffect(() => {
-        if (!pageNumberUrlParam || !totalPages) {
-            return;
-        }
-
-        if (!/\d+/.test(pageNumberUrlParam) || +pageNumberUrlParam <= 0) {
-            navigate('/1');
-            return;
-        }
-
-        if (+pageNumberUrlParam > totalPages) {
-            navigate(`/${totalPages}`);
-            return;
-        }
-
-        setActivePageNumber(+pageNumberUrlParam);
-    }, [pageNumberUrlParam, totalPages, navigate]);
-
-    return { activePageNumber };
-};
-
-export const useFiltering = () => {
-    const [searchParams] = useSearchParams();
-    const [searchText, setSearchText] = useState<string>('');
-    const [sortState, setSortState] = useState<SortModel | null>(null);
-
-    useEffect(() => {
-        const searchParam = searchParams.get('search');
-        const sortName = searchParams.get('sort') as CatBreedSortFields;
-        const sortDirection = searchParams.get('direction');
-
-        if (sortName && sortDirection) {
-            const sort: SortModel = {
-                field: sortName,
-                direction: sortDirection,
-            };
-            setSortState(sort);
-        }
-        setSearchText(searchParam ?? '');
-    }, [searchParams]);
-
-    return { searchText, sortState };
-};
-
-export default function HomePage() {
-    const navigate = useNavigate();
-
-    const [displayData, setDisplayData] = useState<CatBreedModel[]>();
-    const [totalPages, setTotalPages] = useState<number>(0);
-    const { activePageNumber } = useNavigation(totalPages);
-    const { searchText, sortState } = useFiltering();
-
-    const { isLoading, data: queryData } = useQuery('cat-breeds', async () => {
-        const response = await axios.get<CatBreedModel[]>(
-            'https://api.thecatapi.com/v1/breeds'
-        );
-
-        return response.data;
-    });
-
     const handleNavigation = (
         pageNumber: number,
         searchValue: string | null,
@@ -94,20 +34,79 @@ export default function HomePage() {
         navigate(url);
     };
 
-    const handleSearch = (searchValue: string) => {
-        handleNavigation(1, searchValue, sortState);
-    };
+    useEffect(() => {
+        if (!pageNumberUrlParam || !totalPages) {
+            return;
+        }
 
-    const handlePageChange = (pageNumber: number) => {
-        handleNavigation(pageNumber, searchText, sortState);
-    };
+        if (!/\d+/.test(pageNumberUrlParam) || +pageNumberUrlParam <= 0) {
+            navigate('/1');
+            return;
+        }
 
-    const handleSortChange = (sort: SortModel) => {
-        handleNavigation(activePageNumber, searchText, sort);
-    };
+        if (+pageNumberUrlParam > totalPages) {
+            navigate(`/${totalPages}`);
+            return;
+        }
+
+        setActivePageNumber(+pageNumberUrlParam);
+    }, [pageNumberUrlParam, totalPages, navigate]);
+
+    return { activePageNumber, handleNavigation };
+};
+
+export const usePaging = (
+    fetchedData: CatBreedModel[] | undefined,
+    activePageNumber: number
+) => {
+    const [displayData, setDisplayData] = useState<CatBreedModel[]>();
 
     useEffect(() => {
-        if (queryData && activePageNumber) {
+        if (fetchedData && activePageNumber) {
+            const dataSlice = fetchedData.slice(
+                activePageNumber * 10 - 10,
+                activePageNumber * 10
+            );
+
+            setDisplayData(dataSlice);
+        }
+    }, [fetchedData, activePageNumber]);
+
+    return { displayData };
+};
+
+export const useFetching = () => {
+    const [searchParams] = useSearchParams();
+    const [searchText, setSearchText] = useState<string>('');
+    const [sortState, setSortState] = useState<SortModel | null>(null);
+    const [filteredData, setFilteredData] = useState<CatBreedModel[]>();
+    const [totalPages, setTotalPages] = useState<number>(0);
+
+    const { isLoading, data: queryData } = useQuery('cat-breeds', async () => {
+        const response = await axios.get<CatBreedModel[]>(
+            'https://api.thecatapi.com/v1/breeds'
+        );
+
+        return response.data;
+    });
+
+    useEffect(() => {
+        const searchParam = searchParams.get('search');
+        const sortName = searchParams.get('sort') as CatBreedSortFields;
+        const sortDirection = searchParams.get('direction');
+
+        if (sortName && sortDirection) {
+            const sort: SortModel = {
+                field: sortName,
+                direction: sortDirection,
+            };
+            setSortState(sort);
+        }
+        setSearchText(searchParam ?? '');
+    }, [searchParams]);
+
+    useEffect(() => {
+        if (queryData) {
             let filteredData = queryData;
 
             if (searchText) {
@@ -121,17 +120,33 @@ export default function HomePage() {
             if (sortState) {
                 filteredData = sortData(filteredData, sortState);
             }
-
-            const dataSlice = filteredData?.slice(
-                activePageNumber * 10 - 10,
-                activePageNumber * 10
-            );
             setTotalPages(
                 filteredData ? Math.ceil(filteredData.length / 10) : 1
             );
-            setDisplayData(dataSlice);
+            setFilteredData(filteredData);
         }
-    }, [queryData, activePageNumber, searchText, sortState]);
+    }, [queryData, searchText, sortState]);
+
+    return { searchText, sortState, totalPages, filteredData, isLoading };
+};
+
+export default function HomePage() {
+    const { searchText, sortState, totalPages, filteredData, isLoading } =
+        useFetching();
+    const { activePageNumber, handleNavigation } = useNavigation(totalPages);
+    const { displayData } = usePaging(filteredData, activePageNumber);
+
+    const handleSearch = (searchValue: string) => {
+        handleNavigation(1, searchValue, sortState);
+    };
+
+    const handlePageChange = (pageNumber: number) => {
+        handleNavigation(pageNumber, searchText, sortState);
+    };
+
+    const handleSortChange = (sort: SortModel) => {
+        handleNavigation(activePageNumber, searchText, sort);
+    };
 
     return (
         <div className={`${css.appContainer}`}>
